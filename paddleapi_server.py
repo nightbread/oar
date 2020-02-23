b50b7ef265c61401599c40f4e1948a5bc7e5d444
nightbread <nightbread@inbox.lv>
2020-02-23 04:58:10 -0500

    Minor improvements

--- paddleapi_server.py
+++ paddleapi_server.py
@@ -1,44 +1,59 @@
-#!/usr/bin/env python3
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import ssl, urllib.parse, json
-import uuid, time
+#!/usr/bin/env python
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+import argparse
+import json
+import logging
+import ssl
+import sys
+import urllib.parse
+import uuid
 
-class PaddleApiServerHandler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
+class PaddleAPIServerHandler(BaseHTTPRequestHandler):
+    def do_GET(self) -> None:
         self.send_response(200)
         self.end_headers()
         self.wfile.write('This is an activation server for GPGMail.'.encode())
 
-    def do_POST(self):
-        body = self.rfile.read(int(self.headers['Content-Length'], 0)).decode()
+    def do_POST(self) -> None:
+        body = self.rfile.read(int(self.headers['Content-Length'])).decode()
         self.send_response(200)
         self.end_headers()
-
-        print('Receive a POST request from %s:%d -->' % self.client_address)
         request_info = urllib.parse.parse_qs(body)
+        self.log_message('Request data:')
         for key in request_info.keys():
-            print('%-32s =' % key, request_info[key])
-        print()
+            self.log_message('%-32s = %s', key, ' '.join(request_info[key]))
+        self.wfile.write(
+            json.dumps(
+                dict(
+                    success=1,
+                    response=dict(
+                        # Can also be 'feature_license'. Not tested
+                        type='activation_license',
+                        product_id=request_info['product_id'][0],
+                        activation_id=uuid.uuid4().hex,  # Can be anything
+                        # If set to 1, 'expiry_date' field is required.
+                        # Otherwise, license never expires.
+                        expires=0,
+                        # expiry_date=time.time() + 24 * 60 * 60 * 365,
+                    ))).encode())
 
-        response_info = {}
-        response_info['success'] = 1
-        response_info['response'] = {}
-        response_info['response']['product_id'] = request_info['product_id'][0]
-        response_info['response']['activation_id'] = str(uuid.uuid4())      # it can be anything
-        response_info['response']['expires'] = 0                            # if set by 1, 'expiry_date' field is required.
-                                                                            # Otherwise, license never expires.
-        # response_info['response']['expiry_date'] = time.time() + 24 * 60 * 60 * 365       # expires after a year
-        response_info['response']['type'] = 'activation_license'            # it also can be 'feature_license'. Not tested
-        self.wfile.write(json.dumps(response_info).encode())
 
-IP = '127.0.0.1'
-Port = 443
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-k', '--key-file', default='./cert-key.pem')
+    parser.add_argument('-c', '--cert-file', default='./cert-crt.pem')
+    parser.add_argument('-P', '--port', default=443, type=int)
+    parser.add_argument('--ip', default='', metavar='IP')
+    args = parser.parse_args()
+    httpd = ThreadingHTTPServer((args.ip, args.port), PaddleAPIServerHandler)
+    httpd.socket = ssl.wrap_socket(httpd.socket,
+                                   keyfile=args.key_file,
+                                   certfile=args.cert_file,
+                                   server_side=True)
+    httpd.serve_forever()
+    return 0
 
-httpd = HTTPServer((IP, Port), PaddleApiServerHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket, 
-                               keyfile = 'cert-key.pem', 
-                               certfile = 'cert-crt.pem', 
-                               server_side = True)
-httpd.serve_forever()
 
+if __name__ == '__main__':
+    sys.exit(main())
